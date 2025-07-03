81703b0ee1aef451fdea380af95621f474fd4fde
Victor Shilin <chrono.monochrome@gmail.com>
2025-07-03 16:34:10 +0300

    Fix debug mode setting

--- app/api.py
+++ app/api.py
@@ -36,7 +36,7 @@ def handle_uncaught_exception(e):
     print(f"UNCAUGHT EXCEPTION: {e}\n{full_traceback}")
 
     import os
-    is_debug_mode = os.environ.get('FLASK_DEBUG') == '1' or api.app.debug
+    is_debug_mode = os.environ.get('FLASK_DEBUG') == '1'
 
     response = {
         'message': 'An unexpected internal server error occurred.',
