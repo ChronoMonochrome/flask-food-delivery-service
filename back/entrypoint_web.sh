61fe9e28c7ef40c7b54adc66ee58def47bf72384
Victor Shilin <chrono.monochrome@gmail.com>
2025-07-09 16:18:08 +0300

    Correct web entrypoint

--- back/entrypoint_web.sh
+++ back/entrypoint_web.sh
@@ -2,7 +2,7 @@
 # entrypoint_web.sh
 
 echo "Running Flask database migrations..."
-flask db upgrade
+exec flask db upgrade
 
 echo "Starting Gunicorn..."
 exec gunicorn -w 4 -b 0.0.0.0:8011 --access-logfile - --error-logfile - app:app
