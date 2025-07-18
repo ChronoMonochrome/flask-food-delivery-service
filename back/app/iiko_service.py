249f58e28d21bb4a59f552a19c71d09231bacc0e
Victor Shilin <chrono.monochrome@gmail.com>
2025-07-18 15:36:29 +0300

    Update and use mock IIKO API

--- back/app/iiko_service.py
+++ back/app/iiko_service.py
@@ -17,7 +17,8 @@ from diskcache import Cache
 # Data will expire after 900 seconds (15 minutes)
 cache = Cache('iiko_cache', expire=900)
 
-IIKO_API_URL = os.getenv("IIKO_API_URL")
+#IIKO_API_URL = os.getenv("IIKO_API_URL")
+IIKO_API_URL = "http://mock_iiko:5000"
 IIKO_API_TOKEN = os.getenv("IIKO_API_TOKEN")
 
 RECOMMENDATION_CATEGORY_NAME = "Рекомендованные (сиутативные)"
